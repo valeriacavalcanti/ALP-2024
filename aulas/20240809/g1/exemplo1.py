fbadba892df48373c896b715639ce7b3affc1bcf
Valéria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2024-08-15 22:33:37 -0300

    Aula 09/08/2024

---
+++ aulas/20240809/g1/exemplo1.py
@@ -0,0 +1,13 @@
+#nome = 'Valéria Maria'
+#idade = 15
+
+nome = input('Nome: ')
+idade = input('Idade: ')
+idade = int(idade)
+
+dobro_idade = idade * 2
+
+print(nome)
+print(idade)
+print(nome, idade)
+print(dobro_idade)
---
+++ aulas/20240809/g2/exemplo1.py
@@ -0,0 +1,16 @@
+#nome = 'Valéria Maria'
+#idade = 15
+
+nome = input('Nome: ')
+
+idade = input('Idade: ')
+idade = int(idade)
+
+#idade = int(input('Idade: '))
+
+dobro_idade = idade * 2
+
+print(nome)
+print(idade)
+print(nome, idade)
+print(dobro_idade)
